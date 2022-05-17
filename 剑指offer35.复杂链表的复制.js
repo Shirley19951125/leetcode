92055e564004006a6b164dbe622336ed1c5e2566
Shirley19951125 <92510649+Shirley19951125@users.noreply.github.com>
2022-05-17 11:56:41 +0800

    Add files via upload

--- 剑指offer35.复杂链表的复制.js
+++ 剑指offer35.复杂链表的复制.js
@@ -7,3 +7,24 @@ var copyRandomList = function(head,map=new Map()) {
     }
     return map.get(head)
 };
+
+//用迭代实现
+var copyRandomList = function (head) {
+   if(head==null) return null
+   //拷贝.next指针
+    for(let node=head; node!==null; node=node.next.next){
+        const newNode=new Node(node.val,node.next)
+        node.next=newNode
+    }
+    //拷贝.random指针，考虑random为空的情况
+    for(let node=head; node!==null; node=node.next.next ){
+        node.next.random=node.random?node.random.next:null
+    }
+    const newHead=head.next
+     for(let node=head ; node!==null ; node=node.next){
+        const newNode=node.next
+        node.next=node.next.next
+        newNode.next=newNode.next?newNode.next.next : null
+     }
+     return newHead
+};
