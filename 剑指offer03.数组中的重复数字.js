8dc00d7c6650ffd49526d4ce705d776f94618f04
Shirley19951125 <92510649+Shirley19951125@users.noreply.github.com>
2022-05-18 10:13:12 +0800

    Add files via upload

---
+++ 剑指offer03.数组中的重复数字.js
@@ -0,0 +1,9 @@
+var findRepeatNumber = function(nums) {
+    let map={}
+    for(let i=0;i<nums.length;i++){
+        if(!map[nums[i]]){
+            map[nums[i]]=1
+        }else
+        return nums[i]
+    }
+};
