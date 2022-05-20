fb456900fc36f89f1ce30ff3a91d99a3430ffbc8
Shirley19951125 <92510649+Shirley19951125@users.noreply.github.com>
2022-05-20 09:28:58 +0800

    Add files via upload

---
+++ 剑指offer26.树的子结构.js
@@ -0,0 +1,19 @@
+var isSubStructure = function(A, B) {
+    if(!A || !B)
+        return false;
+
+    return isSame(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B);
+};
+
+function isSame(A, B) {
+    // B空了，说明B树遍历完了，是子树
+    if(!B)
+        return true;
+    // 不符合上面情况的前提下A空了，说明B树还没遍历完，不是子树
+    if(!A)
+        return false;
+    // 值不同，不是子树
+    if(A.val !== B.val)
+        return false;
+    return isSame(A.left, B.left) && isSame(A.right, B.right);
+}
