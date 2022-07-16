c568f162f6f7747e35f0454cb7ed8b4262aafb02
Shirley19951125 <92510649+Shirley19951125@users.noreply.github.com>
2022-07-16 21:49:02 +0800

    Add files via upload

---
+++ 1. 两数之和.js
@@ -0,0 +1,11 @@
+var twoSum = function(nums, target) {
+    const map={};
+    for(let i=0;i<nums.length;i++)
+   {
+       if((typeof map[nums[i]])!=='undefined')
+       {
+       return [map[nums[i]],i];}
+       else
+       map[target-nums[i]]=i;
+   }
+};
