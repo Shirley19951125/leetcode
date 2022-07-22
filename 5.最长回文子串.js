a3319ab7ceb74b27b33f0874a50dc5fd4d5ef057
Shirley19951125 <92510649+Shirley19951125@users.noreply.github.com>
2022-07-22 10:46:27 +0800

    Add files via upload

--- 5.最长回文子串.js
+++ 5.最长回文子串.js
@@ -1,23 +1,20 @@
-function longestSubstring(s){
-    let dp=[],res=''
-     for(let i=0;i<s.length;i++){
-         dp[i]=[]
-         for(let j=0;j<=i;j++){
-             if(i===j){
-                 dp[i][j]=1
-             }
-             else if(i-j===1 && s[i]===s[j]){
-                 dp[i][j]=1
-             }
-             else if(dp[i-1][j+1]===1 && s[i]===s[j]){
-                 dp[i][j]=1
-             }
-             if(dp[i][j]===1 && i-j+1>res.length){
-                 res=s.slice(j,i+1)
-             }
-         }
-
-     }
-     return res
- }
- console.log(longestSubstring('abcbaddd'))
+function longestSub(s){
+    let dp=[],l=0
+    for(let i=0;i<s.length;i++){
+        dp[i]=[]
+        for(let j=0;j<=i;j++){
+            if(i==j){
+                dp[i][j]=true
+            }else if(i-j===1 && s[i]===s[j]){
+                dp[i][j]=true
+            }else if(dp[i-1][j+1] && s[i]===s[j]){
+                dp[i][j]=true
+            }
+            if(dp[i][j]){
+                l=Math.max(l,i-j+1)
+            }
+        }
+    }
+    return l
+}
+console.log(longestSub(''))
