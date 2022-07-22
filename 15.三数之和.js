bd26ed175829cbca3879086a0b2a6342c63fa71d
Shirley19951125 <92510649+Shirley19951125@users.noreply.github.com>
2022-07-22 14:39:46 +0800

    Add files via upload

--- 15.三数之和.js
+++ 15.三数之和.js
@@ -1,27 +1,21 @@
-var threeSum = function(nums) {
+function threeSum(nums){
+    res=[]
     nums.sort((a,b)=>a-b)
-    let res=[]
-    for(let i=0;i<nums.length-2;i++){
-        //第一个值为正数时，和必然不能为0
-        if(nums[i]>0) break //避免产生重复数组
-       if(i >=1 && nums[i-1]===nums[i]) continue
-        let j=i+1,k=nums.length-1;
-        let sum
+    for(let i=0;i<nums.length-1;i++){
+        if(nums[i]>0) break
+        if(i>=1 && nums[i]===nums[i-1]) continue
+        let j=i+1,k=nums.length-1
         while(j<k){
             let l=nums[j],r=nums[k]
-           sum =nums[i]+nums[j]+nums[k]
-           //console.log(sum)
-            if(sum>0){
-                k--
-            }else if(sum<0){
-                j++
-            }else{
+            if(nums[i]+nums[j]+nums[k]===0){
                 res.push([nums[i],nums[j],nums[k]])
-                while(j<k && nums[j]===l) j++   // 避免产生重复数组
-                while(j<k && nums[k]==r) k--    //避免产生重复数组
-            }
+                while(j<k && nums[j]===l) j++
+                while(j<k && nums[k]===r) k--
+            }else if(nums[i]+nums[j]+nums[k]<0){
+                j++
+            }else
+            k--
         }
     }
     return res
-};
-console.log(threeSum([-1,0,1,2,-1,-4]))
+}
