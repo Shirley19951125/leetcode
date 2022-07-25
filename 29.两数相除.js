b46190dd48b1a4795297c07fdf13786a99b04914
Shirley19951125 <92510649+Shirley19951125@users.noreply.github.com>
2022-07-25 09:15:18 +0800

    Add files via upload

--- 29.两数相除.js
+++ 29.两数相除.js
@@ -1,12 +1,10 @@
-var divide = function(dividend, divisor) {
-    const max = Math.pow(2, 31) - 1, min = -Math.pow(2, 31);
-    let flag=dividend>0 && divisor<0 || dividend<0 && divisor>0
-     dividend=Math.abs(dividend),divisor=Math.abs(divisor)
-
-     //div是被除数，n是除数
-     function recur(div,n){
-        let count=1,temp=n
+function divide(dividend,divisor){
+    const flag=dividend>0 && divisor<0 || dividend<0 && divisor>0
+    const max=Math.pow(2,31)-1,min=Math.pow(-2,31)
+    dividend=Math.abs(dividend),divisor=Math.abs(divisor)
+    function recur(div,n){
         if(div<n) return 0
+        let count=1,temp=n
         while(temp+temp<div){
             count+=count
             temp+=temp
@@ -14,13 +12,10 @@ var divide = function(dividend, divisor) {
         return count+recur(div-temp,n)
     }
     let res=recur(dividend,divisor)
-    if(flag) {
+    if(flag){
         res*=-1
     }
-
-    //判断溢出
     if(res>max) return max
     if(res<min) return min
     return res
-};
-console.log(divide(-2147483648,2))
+}
